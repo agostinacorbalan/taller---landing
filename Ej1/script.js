84cc8f1ff8d71065806c3da1ecaba8271595f941
agoscr <103857156+agostinacorbalan@users.noreply.github.com>
2024-05-12 19:01:12 -0300

    Add files via upload
    modificacion boton, formulario

---
+++ Ej1/script.js
@@ -0,0 +1,40 @@
+console.log("");
+
+//variables
+var input_nombre = document.getElementById("nombre");
+var input_email = document.getElementById("email");
+var input_informacion = document.getElementById("informacion");
+var input_submit = document.getElementById("boton-enviar");
+
+console.log("input_nombre");
+console.log("input_email");
+
+input_submit.addEventListener("click", enviarFormulario);
+
+function enviarFormulario(event){
+    event.preventDefault();
+    console.log("Clickeado");
+    console.log("!");
+
+ var valor_nombre = input_nombre.value;
+ var valor_email = input_email.value;
+ var valor_informacion = input_informacion.value;
+
+ console.log(valor_nombre);
+ console.log(valor_email);
+ console.log(valor_informacion);
+
+ var placeholder_nombre = document.getElementById("nombre-placeholder");
+ var placeholder_email= document.getElementById("email-placeholder");
+
+ placeholder_nombre.innerHTML = valor_nombre;
+ placeholder_email.innerHTML = valor_email;
+ 
+ //Mostrar feedback
+ elemento_feedback = document.getElementById("feedback");
+ elemento_feedback.classList.remove("oculto");
+
+ //Ocultar form
+ elemento_formulario = document.getElementById("formulario");
+ elemento_formulario.classList.add("oculto");
+}
